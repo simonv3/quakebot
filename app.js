0c3e6084f9ec056dc04a951f7ffb0d42b8eccdda
Simon <svansintjan@gmail.com>
2015-10-22 13:07:53 -0700

    Break out things, switch to all hour feed

--- app.js
+++ app.js
@@ -8,40 +8,40 @@ var sys = require('sys'),
     utils = require('./lib/utils'),
     Promises = require('./lib/promises');
 
-var proximity = '10000m';
+var proximity = '1000000';
 
-var feed = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.atom';
+var feed = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.atom';
 
 var watcher = new Watcher(feed);
 
+var fetchFollowersAndSend = function(quakeData) {
+  Promises.getQuakeBotFollowers('yourquakebot')
+    .then(function(data) {
+      Promises.lookupUsersByIds(data).then(function(users) {
+        users.forEach(function(user) {
+          utils.seeIfEarthquakeByUserAndSendDM(user, quakeData, proximity);
+        });
+
+      }, function(err) {
+        console.log('error looking up quakebot users', err);
+      });
+    },
+    function(err) {
+      console.log(err);
+    });
+};
+
 watcher.on('new article', function(quake){
   var quakeData = utils.extractDataFromQuake(quake);
+  fetchFollowersAndSend(quakeData);
 });
-
 
 watcher.run(function(err, quakes) {
   if (err)
     console.error(err);
-
   smallQuakes = [quakes[0]];
   smallQuakes.map(utils.extractDataFromQuake).forEach(function(quakeData) {
-    console.log(quakeData);
-    Promises.getQuakeBotFollowers('yourquakebot')
-      .then(function(data) {
-        Promises.lookupUsersByIds(data).then(function(users) {
-
-          users.forEach(function(user) {
-            utils.seeIfEarthquakeByUser(user, quakeData, proximity);
-          });
-
-        }, function(err) {
-          console.log('error looking up quakebot users', err);
-        });
-      },
-      function(err) {
-        console.log(err);
-      });
-
+    fetchFollowersAndSend(quakeData);
   });
 });
 
